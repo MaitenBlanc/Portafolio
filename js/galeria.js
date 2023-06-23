2ed6faa0a01f1f3ca3130106fd27d8f8e1dd2e85
Your Name <you@example.com>
2023-06-23 15:05:45 -0300

    Portafolio final

---
+++ js/galeria.js
@@ -0,0 +1,43 @@
+const btnCierra = document.querySelector(".btn-cierra");
+const btnAdelanta = document.querySelector(".btn-adelanta");
+const btnRetrocede = document.querySelector(".btn-retrocede");
+
+const imagenes = document.querySelectorAll(".galeria img");
+const lightbox = document.querySelector(".galeria-principal");
+const imagenActiva = document.querySelector(".imagen-activa");
+
+let indiceImagen = 0;
+
+const abreLightbox = (event) => {
+  imagenActiva.src = event.target.src;
+  lightbox.style.display = "flex";
+  indiceImagen = Array.from(imagenes).indexOf(event.target);
+};
+
+imagenes.forEach((imagen) => {
+  imagen.addEventListener("click", abreLightbox);
+});
+
+btnCierra.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+const adelantaImagen = () => {
+  if (indiceImagen === imagenes.length - 1) {
+    indiceImagen = -1;
+  }
+  imagenActiva.src = imagenes[indiceImagen + 1].src;
+  indiceImagen++;
+};
+
+btnAdelanta.addEventListener("click", adelantaImagen);
+
+const retrocedeImagen = () => {
+  if (indiceImagen === 0) {
+    indiceImagen = imagenes.length;
+  }
+  imagenActiva.src = imagenes[indiceImagen - 1].src;
+  indiceImagen--;
+};
+
+btnRetrocede.addEventListener("click", retrocedeImagen);
